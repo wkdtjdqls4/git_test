8c20c97fdccd118261bfe2ac44c639a2ba4752c6
sbjang <wkdtjdqls4@naver.com>
2019-09-01 20:10:19 +0900

    hotfix

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -14,6 +14,8 @@ int main()
 {
     std::cout << "Hello World!\n";
 	std::cout << "Master Version 1.1.0";
+
+	std::cout << "this is hotfix branch";
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
